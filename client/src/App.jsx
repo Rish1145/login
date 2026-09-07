d2933674b8c61067a5bd4ac438408e5d611c902c
Rish1145 <rushikeshbhongade000@gmail.com>
2026-09-07 08:28:18 +0530

    add simple frontend in src/app.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const login = async () => {
+    const response = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      setMessage(`Hello ${data.username}`);
+    } else {
+      setMessage(data.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br />
+
+      <button onClick={login}>
+        Login
+      </button>
+
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default App;
